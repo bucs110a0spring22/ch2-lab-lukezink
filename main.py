9245bd69eb7ad7cf3167b6f4afeba1f53a35c110
Luke Zink <83885670+lukezink@users.noreply.github.com>
2022-04-27 20:38:18 +0000

    Fixed style guidelines to format output with an explanation of values.

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import random
 
 #Part A
 weeks = 16
-print(weeks, type(weeks))
+print("Number of Weeks: "+str(weeks), type(weeks))
 classes = 5
-print(classes, type(classes))
+print("Number of classes: "+str(classes), type(classes))
 tuition = 6000
-print(tuition, type(tuition))
+print("The tuition is: "+str(tuition), type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
-print(cost_per_week, type(cost_per_week))
+print("The cost per week is: "+ str(cost_per_week), type(cost_per_week))
 classes_per_week = 10
-print(classes_per_week, type(classes_per_week))
+print("The classes per week is: " + str(classes_per_week), type(classes_per_week))
 cost_per_class = cost_per_week/classes_per_week
-print("The cost per class is", cost_per_class, "dollars")
-print(cost_per_class, type(cost_per_class))
+print("The cost per class is", str(cost_per_class), "dollars", type(cost_per_class))
 
 #Part B
 mylist = ["carrot", 69, "Barack Obama", 420, "cAPITALISM"]
 randompick = random.choice(mylist)
-print(randompick)
+print("Random pick: "+str(randompick))
